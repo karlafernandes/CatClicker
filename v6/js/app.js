4cdd27801e684dbbb39acd0c0f2e98ac19a99d55
Karla Fernandes <karla@vitaminak.com.br>
2016-01-22 15:57:25 +0200

    feat: using self to represent ViewModel

--- v6/js/app.js
+++ v6/js/app.js
@@ -18,10 +18,13 @@ var Cat = function(){
 }
 
 var ViewModel = function(){	
+	var self = this;
+	
 	this.currentCat = ko.observable( new Cat() );
 	
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		//this.clickCount(this.clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
 }
 
