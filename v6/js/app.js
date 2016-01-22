244a67bb8b0c5a47be7aa8763747767ba2cdef98
Karla Fernandes <karla@vitaminak.com.br>
2016-01-22 16:06:31 +0200

    feat: Getting Ready To Add More Cats

--- v6/js/app.js
+++ v6/js/app.js
@@ -1,8 +1,9 @@
-var Cat = function(){
-	this.clickCount = ko.observable(0);
-	this.name = "Fluffy";
-	this.imgSrc = "images/fluffy.jpg";
-	this.imgAttribution = "Karlinha";
+var Cat = function(data){
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
 	
 	this.level = ko.computed(function() {
         if (this.clickCount() < 20) {
@@ -13,14 +14,18 @@ var Cat = function(){
             return "Adult";
         }
     }, this);	
- 
-    this.nickname = ko.observableArray(['Brown', 'KaffeMilch', 'Blau']);
 }
 
 var ViewModel = function(){	
 	var self = this;
 	
-	this.currentCat = ko.observable( new Cat() );
+	this.currentCat = ko.observable( new Cat({
+		clickCount: 0,
+		name: "Fluffy",
+		imgSrc: "images/fluffy.jpg",
+		imgAttribution: "Karlinha",
+		nicknames: ['Brown', 'KaffeMilch', 'Blau']
+	}) );
 	
 	this.incrementCounter = function() {
 		//this.clickCount(this.clickCount() + 1);
