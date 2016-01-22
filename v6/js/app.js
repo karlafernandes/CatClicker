c5e61d570bdd159a0e331da54ddf0739735fd9fb
Karla Fernandes <karla@vitaminak.com.br>
2016-01-22 19:03:44 +0200

    feat: showing list of cats

--- v6/js/app.js
+++ v6/js/app.js
@@ -1,10 +1,42 @@
+var initialCats = [
+        {
+			name: "poplinre",
+            clickCount : 0,
+			nicknames: ['Pop', 'Klein'],
+        },
+        {
+			name: "fluffy",
+            clickCount : 0,
+			nicknames: ['Brown', 'KaffeMilch', 'Blau'],
+        },
+        {
+			name: "chewie",
+            clickCount : 0,
+			nicknames: ['Grey', 'Hide'],
+        },
+        {
+			name: "muffin",
+            clickCount : 0,
+			nicknames: ['Poa', 'Ribot'],
+        },
+        {
+			name: "missy",
+            clickCount : 0,
+			nicknames: ['BlueGreen', 'Weiss'],
+        },
+        {
+			name: "jetske",
+            clickCount : 0,
+			nicknames: ['Couple', 'GelbBrown'],
+        }
+];
+
 var Cat = function(data){
 	this.clickCount = ko.observable(data.clickCount);
 	this.name = ko.observable(data.name);
-	this.imgSrc = ko.observable(data.imgSrc);
-	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.imgSrc = ko.observable("images/" + data.name + ".jpg");
 	this.nicknames = ko.observableArray(data.nicknames);
-	
+
 	this.level = ko.computed(function() {
         if (this.clickCount() < 20) {
             return "Infant";
@@ -13,24 +45,52 @@ var Cat = function(data){
         } else {
             return "Adult";
         }
-    }, this);	
+    }, this);
 }
 
 var ViewModel = function(){	
 	var self = this;
 	
-	this.currentCat = ko.observable( new Cat({
-		clickCount: 0,
-		name: "Fluffy",
-		imgSrc: "images/fluffy.jpg",
-		imgAttribution: "Karlinha",
-		nicknames: ['Brown', 'KaffeMilch', 'Blau']
-	}) );
+	this.catList = ko.observableArray([]);
+	
+	initialCats.forEach(function( catItem ){
+		self.catList.push( new Cat( catItem ) );
+	});
+	
+	this.currentCat = ko.observable( this.catList()[0] );
 	
 	this.incrementCounter = function() {
 		//this.clickCount(this.clickCount() + 1);
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
+	
+	this.changeCat = function(catNow) {
+		var catButton = document.getElementById(catNow.name());
+		var oldButton = document.getElementsByName('active')[0];
+		
+		//console.log(catButton);
+		//console.log(oldButton);
+		
+		if (oldButton == undefined) {			
+			self.addClass(catButton);
+		} else {
+			self.removeClasse(oldButton);
+			self.addClass(catButton);		
+		}
+				
+		self.currentCat(catNow);
+	};	
+	
+	this.addClass = function(catButton) {		
+		catButton.setAttribute('name', 'active');
+		catButton.classList.add('active');
+	};
+
+	this.removeClasse = function(catButton) {
+		catButton.setAttribute('name', 'inactive');
+		catButton.classList.remove('active');
+	};
+	
 }
 
 ko.applyBindings(new ViewModel());
